a40dc23de557c4c321222139a0ca31a0b62980f9
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-07-13 12:42:11 +0600

    type intros

--- 0.types_intro.py
+++ 0.types_intro.py
@@ -22,3 +22,37 @@ def get_full_name(first_name: str, last_name: str):
 
 print(get_full_name("fahad", "monshi"))
 
+"""
+Now code editor knows the types and i can suggest related methods and also provides error checks.
+"""
+
+
+
+# For some additional use cases, you might need to import some things from the standard library typing module, for example when you want to declare that something has "any type", you can use Any from typing:
+from typing import Any
+
+def some_function(data: Any):
+	print(data)
+
+
+
+# Generic types
+"""
+Some types can take "type parameters" in square brackets, to define their internal types, for example a "list of strings" would be declared list[str].
+
+These types that can take type parameters are called Generic types or Generics.
+
+You can use the same builtin types as generics (with square brackets and types inside):
+
+- list
+- tuple
+- set
+- dict
+"""
+def process_items(items: list[str]):
+	for item in items:
+		print(item)
+"""
+That means: "the variable items is a list, and each of the items in this list is a str".
+By doing that, your editor can provide support even while processing items from the list
+"""
