777716458272ba6900a1362e6232ca25b599123f
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-07-15 18:58:32 +0600

    Simple body parsing using pydantic [Done]

--- 2. Request body/main.py
+++ 2. Request body/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    name: str
+    age: int
+
 @app.post("/create-user/")
-def create_user(user: dict):
+def create_user(user: User):
     return {
         "success": True,
         "message": "User created successfully",
