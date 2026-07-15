966a98f58c7f425fe26a33791e50bfd74416a577
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-07-15 18:24:34 +0600

    Path parameters done

--- 1. Routes & Basic configuration/main.py
+++ 1. Routes & Basic configuration/main.py
@@ -21,3 +21,23 @@ def users():
     return {
         "users": ["Fahad", "Monshi"]
     }
+
+
+# -----------------------------------------------------
+# Dynamic Routing
+# -----------------------------------------------------
+
+# Path Parameters
+# /users/1/
+# /users/2/
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {
+        "user_id": user_id
+    }
+
+@app.get('/users/{user_name}/')
+def get_user(user_name: str):
+    return {
+        "user_name": user_name
+    }
