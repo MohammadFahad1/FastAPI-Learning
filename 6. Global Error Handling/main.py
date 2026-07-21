015da57299f3ccf77ae6ac30d0bd3ffb9d9f0d8d
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-07-21 19:10:11 +0600

    custom status code

--- 6. Global Error Handling/main.py
+++ 6. Global Error Handling/main.py
@@ -1,5 +1,37 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.post("/create-user/", status_code=status.HTTP_201_CREATED)
+def create_user():
+    return {
+        "message": "User created successfully",
+        "data": {
+            "name": "Fahad",
+            "age": 27
+        }
+    }
+
+@app.get("/users/", status_code=status.HTTP_200_OK)
+def get_user():
+    return {
+        "status": "success",
+        "data": {
+            "name": "Fahad",
+            "age": 27
+        }
+    }
+
+
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+def get_user_by_id(user_id: int):
+    if user_id != 1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return {
+        "success": True,
+        "name": "Fahad",
+        "age": 27,
+        "message": "User fetched successfully"
+    }
 
